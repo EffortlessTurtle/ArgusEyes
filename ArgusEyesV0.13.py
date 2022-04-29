4376c3b8c4fc933acfe7c343df4eb09ee349568a
EffortlessTurtle <30440042+EffortlessTurtle@users.noreply.github.com>
2022-04-29 08:50:00 -0500

    Update ArgusEyesV0.13.py

--- ArgusEyesV0.13.py
+++ ArgusEyesV0.13.py
@@ -1,4 +1,4 @@
-#       <ArgusEyes, webscraper for news stories, v0.13,Working,EffortlessTurtle>
+#       <ArgusEyes, webscraper for news stories, v0.13,Github,EffortlessTurtle>
 ########################################################################
 #       <TODO>
 # -finish other methods to scrape other sites
